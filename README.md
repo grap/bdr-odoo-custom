cc2cd90f6c426136058d07a4bad1ed2439cecdd2
Sylvain LE GAL <sylvain.legal@grap.coop>
2018-07-23 16:28:44 +0200

    [ADD] bdr_modules : new module to install all modules required by the BdR Project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# bdr-odoo-custom
+[![Build Status](https://travis-ci.org/grap/bdr-odoo-custom.svg?branch=10.0)](https://travis-ci.org/grap/bdr-odoo-custom)
+[![codecov](https://codecov.io/gh/grap/bdr-odoo-custom/branch/10.0/graph/badge.svg)](https://codecov.io/gh/grap/bdr-odoo-custom)
+[![Coverage Status](https://coveralls.io/repos/grap/bdr-odoo-custom/badge.png?branch=10.0)](https://coveralls.io/r/grap/bdr-odoo-custom?branch=10.0)
+[![Code Climate](https://codeclimate.com/github/grap/bdr-odoo-custom/badges/gpa.svg)](https://codeclimate.com/github/grap/bdr-odoo-custom)
+
+
+Addons for BdR
+==============
---
+++ bdr_modules/__manifest__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# Copyright (C) 2018-Today: GRAP (http://www.grap.coop)
+# @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'BdR - All Modules',
+    'version': '10.0.1.0.0',
+    'category': 'Custom',
+    'summary': 'Install all modules required for BdR project',
+    'author': 'GRAP',
+    'website': 'http://www.grap.coop',
+    'license': 'AGPL-3',
+    'depends': [
+        'purchase',
+        'sale',
+    ],
+    'data': [
+    ],
+    'installable': False,
+    'auto_install': True,
+}
